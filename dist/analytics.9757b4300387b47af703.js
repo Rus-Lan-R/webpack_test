27c7e9afa16da94175e0db550c286d96d905606c
ruslan <ruslan.rodionow98@gmail.com>
2021-08-08 20:00:06 +0300

    save

---
+++ dist/analytics.9757b4300387b47af703.js
@@ -0,0 +1,1 @@
+window.analytics=function(){let t=0,e=!1;const c=()=>t++;return document.addEventListener("click",c),{destroy(){document.removeEventListener("click",c),e=!0},getClicks:()=>e?`Analitics is destroyed, total click -> ${t}`:t}}();
